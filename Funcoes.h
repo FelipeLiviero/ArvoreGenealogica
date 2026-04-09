cc8ab3fb25ab584018c419885b2b15da1646ddb7
Antonio J Santos <antoniosantos.x@hotmail.com>
2026-04-08 21:49:31 -0300

    Update Funcoes.h
    Correção de bugs

--- Funcoes.h
+++ Funcoes.h
@@ -15,31 +15,23 @@ typedef struct no {
     char nomePai[100];
     ANIVERSARIO aniversario;
 
-    struct no *ptrPai;         
-    struct no *ptrMae;         
-    struct no *irmaoMaisNovo;  
-    struct no *irmaoMaisVelho; 
+    struct no *ptrPai;
+    struct no *ptrMae;
+    struct no *irmaoMaisNovo;
+    struct no *irmaoMaisVelho;
 } NO, *PONT;
 
 void inicializar   (PONT *raiz);
-
 PONT criarNo       (const char *nome, const char *mae,
                     const char *pai,  ANIVERSARIO niver);
-
 PONT inserirPessoa (PONT raiz);
-
 PONT removerPessoa (PONT raiz, const char *nomeAlvo);
-
 PONT buscarPessoa  (PONT raiz, const char *nome);
-
 void imprimirNo    (PONT p);
-
 void imprimirArvore(PONT raiz);
-
 void imprimirPai   (PONT raiz, const char *nome);
 void imprimirMae   (PONT raiz, const char *nome);
 void imprimirIrmaos(PONT raiz, const char *nome);
-
 void liberarArvore (PONT raiz);
 
-#endif 
+#endif
